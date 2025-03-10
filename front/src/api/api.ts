2d793e9fe723e333f7f048e4c3f8edb3539030f9
Nahuel <70348604+Nahuel77@users.noreply.github.com>
2025-03-10 04:00:07 -0300

    Update api.ts

--- front/src/api/api.ts
+++ front/src/api/api.ts
@@ -1,6 +1,9 @@
 import axios from 'axios';
 
 const API = axios.create({
+    //NOTA A mi Mismo:
+    //HTTP por ser desarrollo local.
+    //Si se desplega a un server configurar certificado SSL
     baseURL: 'http://localhost:5000',
 });
 
